76cecd0f8c2cb2bd6370931efde083781ecd0a09
Cameron Tong <cameron.tong2b@gmail.com>
2022-01-12 13:57:08 -0500

    remove calls to bigdecimal

--- spec/factories/product.rb
+++ spec/factories/product.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :product do
+    number { 88 }
+    code { "widget" }
+    unit_price { 19.99 }
     to_create do |instance|
       DB.save(instance)
       instance
--- spec/refactoring_a_test_spec.rb
+++ spec/refactoring_a_test_spec.rb
@@ -1,12 +1,10 @@
-require "bigdecimal"
-
 RSpec.describe RefactoringATest do
   context "#initialize" do
     it "creates a line item" do
       billing_address = Address.new("1222 1st St SW", "Calgary", "Alberta", "T2N 2V2", "Canada")
       shipping_address = Address.new("1333 1st St SW", "Calgary", "Alberta", "T2N 2V2", "Canada")
-      customer = Customer.new(99, "John", "Doe", BigDecimal("30"), billing_address, shipping_address)
-      product = Product.new(88, "SomeWidget", BigDecimal("19.99"))
+      customer = Customer.new(99, "John", "Doe", 30, billing_address, shipping_address)
+      product = Product.new(88, "SomeWidget", 19.99)
       invoice = Invoice.new(customer: customer)
 
       item = LineItem.new(invoice, product, 10, 15)
@@ -21,8 +19,8 @@ RSpec.describe RefactoringATest do
   end
   context "#add_item_quantity" do
     it "adds an item to an invoice" do
-      customer = create_customer(percent_discount: BigDecimal("30"))
-      product = FactoryBot.create(:product, number: 88, code: "SomeWidget", unit_price: BigDecimal("19.99"))
+      customer = FactoryBot.create(:customer, percent_discount: 30)
+      product = FactoryBot.create(:product)
       invoice = FactoryBot.create(:invoice, customer: customer)
 
       invoice.add_item_quantity(product, 5)
@@ -36,13 +34,5 @@ RSpec.describe RefactoringATest do
         )
       )
     end
-  end
-
-  def create_customer(percent_discount:)
-    FactoryBot.create(:customer, percent_discount: percent_discount)
-  end
-
-  def create_address
-    FactoryBot.create(:address)
   end
 end
