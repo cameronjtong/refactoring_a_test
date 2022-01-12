ecb4c4820fec77133224d903ce2f6c7863e07dad
Cameron Tong <cameron.tong2b@gmail.com>
2022-01-12 14:01:02 -0500

    use factory bot accross both test cases

--- spec/refactoring_a_test_spec.rb
+++ spec/refactoring_a_test_spec.rb
@@ -1,11 +1,9 @@
 RSpec.describe RefactoringATest do
   context "#initialize" do
     it "creates a line item" do
-      billing_address = Address.new("1222 1st St SW", "Calgary", "Alberta", "T2N 2V2", "Canada")
-      shipping_address = Address.new("1333 1st St SW", "Calgary", "Alberta", "T2N 2V2", "Canada")
-      customer = Customer.new(99, "John", "Doe", 30, billing_address, shipping_address)
-      product = Product.new(88, "SomeWidget", 19.99)
-      invoice = Invoice.new(customer: customer)
+      customer = create(:customer, percent_discount: 15)
+      product = create(:product)
+      invoice = create(:invoice, customer: customer)
 
       item = LineItem.new(invoice, product, 10, 15)
 
